6cf849acb17c32ae3d51ee13259a9fe02031905b
KNY2020 <kangningyou@gmail.com>
2021-08-27 14:06:56 +0800

    Update detect_version.h

--- detect_version.h
+++ detect_version.h
@@ -1,2 +1,8 @@
+/*************************************************
+Copyright:	KNY2021
+Author: 	KNY2021
+Date:2021-08-20
+Description:detect_version.h
+**************************************************/
 
 int detect_version(char* url);
